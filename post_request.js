417363b0a81286522fd5124791dc8f94bec6dfda
Albert Martinez <albertmart2002@gmail.com>
2025-05-20 22:14:15 -0400

    Notes.txt

--- post_request.js
+++ post_request.js
@@ -9,10 +9,3 @@ fetch("https://apply-to-avantos.dev-sandbox.workload.avantos-ai.net", {
 })
   .then((response) => response.json())
   .then((json) => console.log(json));
-
-
-/*{
-    Challenge_link: "https://fluttering-atmosphere-1b5.notion.site/Journey-Builder-React-Coding-Challenge-190d5fe264fa80cba39ec21afc6d42ec",
-    Instructions: "Submit your solutions as a link to a github repo named `5471cd` to the following email address: challenge-request-sub-aaaapriy22ktilybsd6gv7beiq@avantos.slack.com. 
-    Questions can be sent to challenge-help-aaaaprjeraoxiaa2wlnlda7vsi@avantos.slack.com"
-}*/
